c8fea89ed79ec18b212fc69bef10f3bb9b7a7521
Fergus Cooper <fcooper8472@gmail.com>
2019-04-19 23:47:58 +0100

    Adding temporary functionality for running hourly builds

---
+++ cron_azure_ci.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+# Commit a change to the azure-ci branch of this repo on pints@skip to trigger the next test.
+# This will become unnecessary once azure-pipelines yaml config supports scheduled triggers.
+# This script is called from a crontab on pints@skip and runs from a local version of this repo.
+
+git checkout azure-ci
+git pull
+date >> poke-azure.log
+git add poke-azure.log
+git commit -m "poking azure"
+git push
