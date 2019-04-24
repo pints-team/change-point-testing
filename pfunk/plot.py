4040456a7c725b5b82bc26a936c97829dacf4ac5
Michael Clerx <michael.clerx@cs.ox.ac.uk>
2019-04-24 14:53:00 +0100

    Made plot methods more robust against lack of data.

--- pfunk/plot.py
+++ pfunk/plot.py
@@ -30,18 +30,21 @@ def variable(results, variable, title, ylabel, threshold=None):
     plt.xlabel('Commit')
     fig.autofmt_xdate()
     x, y, u, m, s = pfunk.gather_statistics_per_commit(results, variable)
-    xlookup = dict(zip(u, np.arange(len(u))))
-    x = np.array([xlookup[i] for i in x], dtype=float)
-    x += np.random.uniform(-r, r, x.shape)
-    plt.plot(u, m, 'ko-', alpha=0.5)
-    plt.plot(x, y, 'x', alpha=0.75)
-    if threshold:
-        plt.axhline(threshold)
-    try:
-        y = np.array(y)
-        ymax = np.max(y[np.isfinite(y)])
-    except ValueError:
-        ymax = 1
+    if len(x) == 0:
+        plt.text(0.5, 0.5, 'No data')
+    else:
+        xlookup = dict(zip(u, np.arange(len(u))))
+        x = np.array([xlookup[i] for i in x], dtype=float)
+        x += np.random.uniform(-r, r, x.shape)
+        plt.plot(u, m, 'ko-', alpha=0.5)
+        plt.plot(x, y, 'x', alpha=0.75)
+        if threshold:
+            plt.axhline(threshold)
+        try:
+            y = np.array(y)
+            ymax = np.max(y[np.isfinite(y)])
+        except ValueError:
+            ymax = 1
 
     # Right plot: Same, but with outliers removed, to achieve a "zoom" on the
     # most common, recent data.
@@ -51,26 +54,29 @@ def variable(results, variable, title, ylabel, threshold=None):
     fig.autofmt_xdate()
     x, y, u, m, s = pfunk.gather_statistics_per_commit(
         results, variable, remove_outliers=True, n=10)
-    xlookup = dict(zip(u, np.arange(len(u))))
-    x = np.array([xlookup[i] for i in x], dtype=float)
-    x += np.random.uniform(-r, r, x.shape)
-    plt.plot(u, m, 'ko-', alpha=0.5)
-    plt.plot(x, y, 'x', alpha=0.75)
-    if threshold:
-        # Show threshold line only if it doesn't change the zoom
-        if threshold <= np.max(y) and threshold >= np.min(y):
-            plt.axhline(threshold)
-    y = np.array(y)
-    try:
-        y = np.array(y)
-        ymax = max(ymax, np.max(y[np.isfinite(y)]))
-    except ValueError:
-        pass
-
-    if ymax > 1000:
-        plt.subplots_adjust(0.1, 0.16, 0.99, 0.92, 0.2, 0)
+    if len(x) == 0:
+        plt.text(0.5, 0.5, 'No data')
     else:
-        plt.subplots_adjust(0.07, 0.16, 0.99, 0.92, 0.17, 0)
+        xlookup = dict(zip(u, np.arange(len(u))))
+        x = np.array([xlookup[i] for i in x], dtype=float)
+        x += np.random.uniform(-r, r, x.shape)
+        plt.plot(u, m, 'ko-', alpha=0.5)
+        plt.plot(x, y, 'x', alpha=0.75)
+        if threshold:
+            # Show threshold line only if it doesn't change the zoom
+            if threshold <= np.max(y) and threshold >= np.min(y):
+                plt.axhline(threshold)
+        y = np.array(y)
+        try:
+            y = np.array(y)
+            ymax = max(ymax, np.max(y[np.isfinite(y)]))
+        except ValueError:
+            pass
+
+        if ymax > 1000:
+            plt.subplots_adjust(0.1, 0.16, 0.99, 0.92, 0.2, 0)
+        else:
+            plt.subplots_adjust(0.07, 0.16, 0.99, 0.92, 0.17, 0)
 
     return fig
 
