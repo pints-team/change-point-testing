4ae16500ab74339a7c945e3341ae62b5ee42f148
Michael Clerx <michael.clerx@cs.ox.ac.uk>
2019-05-15 12:23:21 +0100

    Trying to make azure include test name

--- cron_azure_ci.sh
+++ cron_azure_ci.sh
@@ -4,9 +4,12 @@
 # This will become unnecessary once azure-pipelines yaml config supports scheduled triggers.
 # This script is called from a crontab on pints@skip and runs from a local version of this repo.
 
+# Get next test name
+NEXT="$(./funk list --next)"
+
 git checkout azure-ci
 git pull
 date >> poke-azure.log
 git add poke-azure.log
-git commit -m "poking azure"
+git commit -m "Preparing to run ${NEXT}"
 git push
