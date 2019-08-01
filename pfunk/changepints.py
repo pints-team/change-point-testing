aa4fbb1be21fa8e2d71da374c7f49b7496aada4e
Abhishek Dasgupta <abhishek.dasgupta@cs.ox.ac.uk>
2019-08-01 13:41:07 +0100

    add changepints

---
+++ pfunk/changepints.py
@@ -0,0 +1,26 @@
+import numpy as np
+import ruptures as rpt
+import matplotlib.pyplot as plt
+
+class ChangePints:
+
+    def __init__(self, model="rbf", penalty=3):
+        self.model = model
+        self.penalty = penalty
+
+    def data(self, source):
+        self.signal = np.array(source)
+        algo = rpt.Pelt(model=self.model).fit(self.signal)
+        self.bkpts = algo.predict(pen=self.penalty)
+        return self
+
+    def breakpoints(self):
+        return self.bkpts
+
+    def crossed_threshold(self, nbkpts=1):
+        "If number of breakpoints > nbkpts, return False"
+        return len(self.breakpoints()) > nbkpts
+
+    def show(self):
+        rpt.display(self.signal, self.breakpoints())
+        plt.show()
