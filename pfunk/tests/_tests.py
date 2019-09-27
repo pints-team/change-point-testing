2061ace3928294a66c94765417196fced45aa17a
Michael Clerx <michael.clerx@cs.ox.ac.uk>
2019-09-27 11:49:54 +0100

    Added new methods to functional testing.

--- pfunk/tests/_tests.py
+++ pfunk/tests/_tests.py
@@ -39,83 +39,92 @@ def analyse(name, database):
     return _tests[name].analyse(database)
 
 
-def results_writer_generator(name, date, path, identifier=None):
-    """ Returns a ResultsDatabaseWriter. """
+def rwg(name, date, path, identifier=None):
+    """ Generator that returns a ResultsDatabaseWriter. """
     return pfunk.ResultsDatabaseWriter(path, name, date, identifier)
 
 
 from .optimisation import OptimisationLogistic
-add(OptimisationLogistic(results_writer_generator, 'CMAES', 1.0))
-add(OptimisationLogistic(results_writer_generator, 'XNES', 1.0))
-add(OptimisationLogistic(results_writer_generator, 'SNES', 1.0))
-add(OptimisationLogistic(results_writer_generator, 'PSO', 1.0))
+add(OptimisationLogistic(rwg, 'CMAES', 1.0))
+add(OptimisationLogistic(rwg, 'XNES', 1.0))
+add(OptimisationLogistic(rwg, 'SNES', 1.0))
+add(OptimisationLogistic(rwg, 'PSO', 1.0))
+add(OptimisationLogistic(rwg, 'NelderMead', 1.0))
 
 
 from .optimisation import OptimisationFN
-add(OptimisationFN(results_writer_generator, 'CMAES', 1.0))
-add(OptimisationFN(results_writer_generator, 'XNES', 1.0))
-add(OptimisationFN(results_writer_generator, 'SNES', 1.0))
-add(OptimisationFN(results_writer_generator, 'PSO', 1.0))
+add(OptimisationFN(rwg, 'CMAES', 1.0))
+add(OptimisationFN(rwg, 'XNES', 1.0))
+add(OptimisationFN(rwg, 'SNES', 1.0))
+add(OptimisationFN(rwg, 'PSO', 1.0))
+add(OptimisationFN(rwg, 'NelderMead', 1.0))
 
 
 from .optimisation import OptimisationBR
-add(OptimisationBR(results_writer_generator, 'CMAES', 2.0))
-add(OptimisationBR(results_writer_generator, 'XNES', 2.0))
-add(OptimisationBR(results_writer_generator, 'SNES', 4.0))
-add(OptimisationBR(results_writer_generator, 'PSO', 2.0))
+add(OptimisationBR(rwg, 'CMAES', 2.0))
+add(OptimisationBR(rwg, 'XNES', 2.0))
+add(OptimisationBR(rwg, 'SNES', 4.0))
+add(OptimisationBR(rwg, 'PSO', 2.0))
+add(OptimisationBR(rwg, 'NelderMead', 2.0))
 
 
 from .mcmc_normal import MCMCNormal
-# Single-chain methods
-add(MCMCNormal(results_writer_generator, 'AdaptiveCovarianceMCMC', 1, 0.05))
-add(MCMCNormal(
-    results_writer_generator, 'HamiltonianMCMC', 1, 0.05, max_iter=1000))
-add(MCMCNormal(results_writer_generator, 'MetropolisRandomWalkMCMC', 1, 0.2))
-add(MCMCNormal(results_writer_generator, 'PopulationMCMC', 1, 1.0))
+add(MCMCNormal(rwg, 'DifferentialEvolutionMCMC', 3, 0.1))
+add(MCMCNormal(rwg, 'DreamMCMC', 3, 0.1))
+add(MCMCNormal(rwg, 'EmceeHammerMCMC', 3, 0.1))
+add(MCMCNormal(rwg, 'HaarioACMC', 1, 0.05))
+add(MCMCNormal(rwg, 'HaarioBardenetACMC', 1, 0.05))
+add(MCMCNormal(rwg, 'HamiltonianMCMC', 1, 0.05, max_iter=1000))
+add(MCMCNormal(rwg, 'MALAMCMC', 1, 1.0))
+add(MCMCNormal(rwg, 'MetropolisRandomWalkMCMC', 1, 0.2))
+add(MCMCNormal(rwg, 'PopulationMCMC', 1, 1.0))
+add(MCMCNormal(rwg, 'RelativisticMCMC', 1, 1.0))
+add(MCMCNormal(rwg, 'SliceDoublingMCMC', 1, 1.0))
+add(MCMCNormal(rwg, 'SliceStepoutMCMC', 1, 1.0))
 # Multi-chain methods
-add(MCMCNormal(results_writer_generator, 'DifferentialEvolutionMCMC', 3, 0.1))
-add(MCMCNormal(results_writer_generator, 'DreamMCMC', 3, 0.1))
-add(MCMCNormal(results_writer_generator, 'EmceeHammerMCMC', 3, 0.1))
 
 
 # issue 518 - turn off banana test for mcmc samplers
 from .mcmc_banana import MCMCBanana
 # Single-chain methods
-add(MCMCBanana(results_writer_generator, 'AdaptiveCovarianceMCMC', 1, 1.0))
-# Requires gradient
-#add(MCMCBanana(results_writer_generator, 'HamiltonianMCMC', 1, 1.0))
-add(MCMCBanana(results_writer_generator, 'MetropolisRandomWalkMCMC', 5, 1.0))
-#add(MCMCBanana(results_writer_generator, 'PopulationMCMC', 1, 1.0, 50000))
-# Multi-chain methods
-#add(MCMCBanana(results_writer_generator, 'DifferentialEvolutionMCMC', 3, 1.0))
-add(MCMCBanana(results_writer_generator, 'DreamMCMC', 4, 1.0))
-add(MCMCBanana(results_writer_generator, 'EmceeHammerMCMC', 3, 1.0))
+add(MCMCBanana(rwg, 'DifferentialEvolutionMCMC', 3, 1.0))
+add(MCMCBanana(rwg, 'DreamMCMC', 4, 1.0))
+add(MCMCBanana(rwg, 'EmceeHammerMCMC', 3, 1.0))
+add(MCMCBanana(rwg, 'HaarioACMC', 1, 1.0))
+add(MCMCBanana(rwg, 'HaarioBardenetACMC', 1, 1.0))
+#add(MCMCBanana(rwg, 'HamiltonianMCMC', 1, 1.0))    # gradient
+add(MCMCBanana(rwg, 'MALAMCMC', 1, 1.0))
+add(MCMCBanana(rwg, 'MetropolisRandomWalkMCMC', 5, 1.0))
+add(MCMCBanana(rwg, 'PopulationMCMC', 1, 1.0, max_iter=50000))
+add(MCMCBanana(rwg, 'RelativisticMCMC', 1, 1.0))
+add(MCMCBanana(rwg, 'SliceDoublingMCMC', 1, 1.0))
+add(MCMCBanana(rwg, 'SliceStepoutMCMC', 1, 1.0))
 
 
 # issue 516 - turn off egg box test for mcmc samplers
 # due to high difficulty of the problem
 #from .mcmc_egg_box import MCMCEggBox
 # Single-chain methods
-#add(MCMCEggBox(results_writer_generator, 'AdaptiveCovarianceMCMC', 1, 1.0))
-#add(MCMCEggBox(results_writer_generator, 'HamiltonianMCMC', 1, 1.0))
-#add(MCMCEggBox(results_writer_generator, 'MetropolisRandomWalkMCMC', 1, 1.0))
-#add(MCMCEggBox(results_writer_generator, 'PopulationMCMC', 1, 1.0))
+#add(MCMCEggBox(rwg, 'AdaptiveCovarianceMCMC', 1, 1.0))
+#add(MCMCEggBox(rwg, 'HamiltonianMCMC', 1, 1.0))
+#add(MCMCEggBox(rwg, 'MetropolisRandomWalkMCMC', 1, 1.0))
+#add(MCMCEggBox(rwg, 'PopulationMCMC', 1, 1.0))
 # Multi-chain methods
-#add(MCMCEggBox(results_writer_generator, 'DifferentialEvolutionMCMC', 6, 1.0))
-#add(MCMCEggBox(results_writer_generator, 'DreamMCMC', 6, 1.0))
+#add(MCMCEggBox(rwg, 'DifferentialEvolutionMCMC', 6, 1.0))
+#add(MCMCEggBox(rwg, 'DreamMCMC', 6, 1.0))
 
 
 from .nested_normal import NestedNormal
-add(NestedNormal(results_writer_generator, 'NestedEllipsoidSampler', 0.16))
-add(NestedNormal(results_writer_generator, 'NestedRejectionSampler', 0.16))
+add(NestedNormal(rwg, 'NestedEllipsoidSampler', 0.16))
+add(NestedNormal(rwg, 'NestedRejectionSampler', 0.16))
 
 
 from .nested_banana import NestedBanana
-add(NestedBanana(results_writer_generator, 'NestedEllipsoidSampler', 0.1))
-add(NestedBanana(results_writer_generator, 'NestedRejectionSampler', 1.0))
+add(NestedBanana(rwg, 'NestedEllipsoidSampler', 0.1))
+add(NestedBanana(rwg, 'NestedRejectionSampler', 1.0))
 
 
 from .nested_egg_box import NestedEggBox
-add(NestedEggBox(results_writer_generator, 'NestedEllipsoidSampler', 0.12))
-add(NestedEggBox(results_writer_generator, 'NestedRejectionSampler', 0.12))
+add(NestedEggBox(rwg, 'NestedEllipsoidSampler', 0.12))
+add(NestedEggBox(rwg, 'NestedRejectionSampler', 0.12))
 
